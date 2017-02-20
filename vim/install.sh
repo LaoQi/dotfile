06fa0ab93d9c0155c278cd32a577459e73343b35
LaoQi <qjqjqzy@gmail.com>
2017-02-20 22:52:38 +0800

    install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+cd `dirname $0`
+VIM_PATH=`pwd`
+
+ln -s $VIM_PATH ~/.vim
+
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/tmp
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim --depth 1
+
+vim ~/.vim/vimrc

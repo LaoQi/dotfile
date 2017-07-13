e918ac397ad4b0229c55cbd414f59f3ac2407d36
john <qjqjqzy@gmail.com>
2017-07-14 00:58:44 +0800

    switched from Vundle to Plug

--- vim/install.sh
+++ vim/install.sh
@@ -4,9 +4,12 @@ VIM_PATH=`pwd`
 
 ln -s $VIM_PATH ~/.vim
 
+mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/tmp
+mkdir -p ~/.vim/colors
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim --depth 1
+wget -o ~/.vim/colors/molokai.vim "https://raw.githubusercontent.com/fatih/molokai/master/colors/molokai.vim"
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim --depth 1
 
 vim ~/.vim/vimrc
